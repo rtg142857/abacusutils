40e28cfe5c7e6f8ea7b6b9c5192dc71951229364
Alexander M. <rtg142857@gmail.com>
2025-03-28 17:28:20 +0000

    Initial changes to prepare_sim; safety commit

---
+++ abacusnbody/data/cosmology.py
@@ -0,0 +1,318 @@
+#! /usr/bin/env python
+# From Alex Smith's HOD_Mock_Pipeline, with modifications for flamingo/peregrinus
+import numpy as np
+import pandas as pd
+from scipy.interpolate import RegularGridInterpolator
+from cosmoprimo.fiducial import AbacusSummit
+from cosmoprimo import Cosmology as Cosmology_cosmoprimo
+import yaml
+import configparser
+
+
+class Cosmology(object):
+    """
+    Class containing useful cosmology methods. Assumes flat LCDM Universe.
+    
+    For specific simulation cosmologies, use the classes provided, e.g.
+    CosmologyAbacus for the AbacusSummit simulation cosmologies.
+
+    Args:
+        cosmo: cosmoprimo cosmology object
+    """
+    def __init__(self, cosmo):
+
+        self.h0     = cosmo.h
+        self.OmegaM = cosmo.Omega_m(0)
+
+        self.cosmo_cosmoprimo = cosmo
+
+        self.__interpolator = self.__initialize_interpolator()
+
+
+    def __initialize_interpolator(self):
+        # create RegularGridInterpolator for converting comoving
+        # distance to redshift
+        z = np.arange(0, 3, 0.0001)
+        rcom = self.comoving_distance(z)
+        return RegularGridInterpolator((rcom,), z,
+                                       bounds_error=False, fill_value=None)
+
+    
+    def H(self, redshift):
+        """
+        Hubble parameter as a function of redshift
+
+        Args:
+            redshift: array of redshift
+        Returns:
+            array of H in units [km/s/Mpc]
+        """
+        return 100 * self.h0 * self.cosmo_cosmoprimo.efunc(redshift)
+    
+
+    def critical_density(self, redshift):
+        """
+        Critical density of the Universe as a function of redshift
+
+        Args:
+            redshift: array of redshift
+        Returns:
+            array of critical density in units [Msun Mpc^-3 h^2]
+        """
+        rho_crit = self.cosmo_cosmoprimo.rho_crit(redshift) * 1e10
+
+        return rho_crit
+
+
+    def mean_density(self, redshift):
+        """
+        Mean matter density of the Universe as a function of redshift
+
+        Args:
+            redshift: array of redshift
+        Returns:
+            array of critical density in units [Msun Mpc^-3 h^2]
+        """
+        # mean density at z=0
+        rho_mean0 = self.critical_density(0) * self.OmegaM
+
+        # evolve to redshift z
+        return  rho_mean0 * (1+redshift)**3
+
+
+    def comoving_distance(self, redshift):
+        """
+        Comoving distance to redshift
+
+        Args:
+            redshift: array of redshift
+        Returns:
+            array of comoving distance in units [Mpc/h]
+        """
+        return self.cosmo_cosmoprimo.comoving_radial_distance(redshift)
+
+
+    def redshift(self, distance):
+        """
+        Redshift to comoving distance
+
+        Args:
+            distance: comoving distance in units [Mpc/h]
+        Returns:
+            array of redshift
+        """
+        return self.__interpolator(distance)
+    
+    
+    def growth_factor(self, z):
+        """
+        Linear growth factor D(a), as a function of redshift
+
+        Args:
+            z: array of redshift
+        Returns:
+            Linear growth factor
+        """
+        return self.cosmo_cosmoprimo.growth_factor(z)
+       
+        
+    def growth_rate(self, z):
+        """
+        Returns the growth rate, f = dln(D)/dln(a)
+
+        Args:
+            z: array of redshift
+        Returns:
+            Growth rate
+        """
+        return self.cosmo_cosmoprimo.growth_rate(z)
+    
+    def dVdz(self, z):
+        """
+        Returns comoving volume element (multiplied by solid angle of full sky)
+        
+        Args:
+            z: array of redshift
+        Returns:
+            Comoving volume element
+        """
+        c    = 3e5 # km/s
+        H100 = 100 # km/s/Mpc
+        return 4*np.pi*(c/H100) * self.comoving_distance(z)**2 / \
+                                self.cosmo_cosmoprimo.efunc(z)
+    
+
+    def age(self, z):
+        return self.cosmo_cosmoprimo.time(z)
+    
+    
+    def get_xi_scaling_factor(self, cosmo_new, r_bins, pimax=120,
+                               correlation_function="xi", scale=8, 
+                               power_spectrum="lin", z=0.2):
+        """
+        Returns the cosmology rescaling factors for the correlation
+        function
+
+        Args:
+            cosmo_new: new cosmology to rescale to
+            r_bins:    array of r bins that xi is evaluated at, in Mpc/h
+            [pimax]:   maximum value of pi in integral, for wp only, in Mpc/h.
+                       Default is 120
+            [correlation_function]: type of correlation function, "xi" or "wp".
+                       Default is "xi"
+            [scale]:   scale below which the scaling factor is set to 1, in Mpc/h.
+                       Default is 8
+            [power_spectrum]: "lin", "nl" or "zel"
+            [z]:       redshift. Default is 0.2
+        """
+
+        from hodpy.power_spectrum import PowerSpectrum
+
+        Pk1 = PowerSpectrum(self)
+        Pk2 = PowerSpectrum(cosmo_new)
+        
+        if correlation_function=="xi":
+            xi_c1 = Pk1.get_xi(r_bins, z=z, power_spectrum=power_spectrum)
+            xi_c2 = Pk2.get_xi(r_bins, z=z, power_spectrum=power_spectrum)
+            xi_c1_8 = Pk1.get_xi(np.array([scale,]), z=z, power_spectrum=power_spectrum)[0]
+            xi_c2_8 = Pk2.get_xi(np.array([scale,]), z=z, power_spectrum=power_spectrum)[0]
+            scaling_factor = xi_c2/xi_c1 * (xi_c1_8/xi_c2_8)
+            
+        elif correlation_function=="wp":
+            wp_c1 = Pk1.get_wp(r_bins, z=z, pimax=pimax, power_spectrum=power_spectrum)
+            wp_c2 = Pk2.get_wp(r_bins, z=z, pimax=pimax, power_spectrum=power_spectrum)
+            wp_c1_8 = Pk1.get_wp(np.array([scale,]), z=z, pimax=pimax, power_spectrum=power_spectrum)[0]
+            wp_c2_8 = Pk2.get_wp(np.array([scale,]), z=z, pimax=pimax, power_spectrum=power_spectrum)[0]
+            scaling_factor = wp_c2/wp_c1 * (wp_c1_8/wp_c2_8)
+            
+        else: 
+            raise ValueError("Invalid correlation function", correlation_function)
+        
+        # keep scaling_factor fixed to 1 below scale
+        scaling_factor[r_bins<scale] = 1.0
+    
+        return scaling_factor
+    
+
+class CosmologyMXXL(Cosmology):
+    """
+    MXXL simulation cosmology
+    """
+    def __init__(self):
+        
+        cosmo_cosmoprimo = Cosmology_cosmoprimo(h=0.73, Omega_cdm=0.25-0.045, Omega_b=0.045,
+                                                sigma8=0.9, n_s=1, engine='class')
+        super().__init__(cosmo_cosmoprimo)
+        
+        
+class CosmologyOR(Cosmology):
+    """
+    OuterRim simulation cosmology
+    """
+    def __init__(self):
+        cosmo_cosmoprimo = Cosmology_cosmoprimo(h=0.71, omega_cdm=0.1109, omega_b=0.02258,
+                                                sigma8=0.8, n_s=0.963, engine='class')
+        super().__init__(cosmo_cosmoprimo)
+        
+        
+class CosmologyUNIT(Cosmology):
+    """
+    UNIT simulation cosmology
+    """
+    def __init__(self):
+        cosmo_cosmoprimo = Cosmology_cosmoprimo(h=0.6774, Omega_cdm=0.3089-0.04860, Omega_b=0.04860,
+                                                sigma8=0.8147, n_s=0.9667, engine='class')
+        super().__init__(cosmo_cosmoprimo)
+        
+        
+class CosmologyUchuu(Cosmology):
+    """
+    Uchuu simulation cosmology
+    """
+    def __init__(self):
+        cosmo_cosmoprimo = Cosmology_cosmoprimo(h=0.6774, Omega_cdm=0.3089-0.04860, Omega_b=0.04860,
+                                                sigma8=0.8159, n_s=0.9667, engine='class')
+        super().__init__(cosmo_cosmoprimo)
+
+    
+
+class CosmologyAbacus(Cosmology):
+    """
+    AbacusSummit simulation cosmology
+
+    Args:
+        cosmo: simulation cosmology number, e.g. 0 for `c000' Planck cosmology
+    """
+    def __init__(self, cosmo):
+        
+        cosmo_cosmoprimo = AbacusSummit(cosmo)
+        
+        super().__init__(cosmo_cosmoprimo)
+        
+        
+    def __get_params(self, cosmo):
+        
+        cosmo_number = np.zeros(len(self.__param_array[:,0]), dtype="i")
+        for i in range(len(self.__param_array[:,0])):
+            cosmo_number[i] = int(self.__param_array[i,0][11:])
+        
+        idx = np.where(cosmo_number==cosmo)[0][0]
+        
+        return np.array(self.__param_array[idx,2:], dtype="f")
+
+class CosmologyFlamingo(Cosmology):
+    """
+    Flamingo simulation cosmology
+
+    Args:
+        path_config_filename: Path to the config file containing paths to other useful things
+    """
+    def __init__(self, path_config_filename):
+        # TODO: Get sigma8 and n_s from the initial condition parameter file (and find where it is)
+        #param_file_path = "/cosma8/data/dp004/flamingo/Runs/L" + str(L) + "N" + str(N) + "/" + simulation + "/used_parameters.yml"
+
+        with open(path_config_filename, "r") as file:
+            path_config = yaml.safe_load(file)
+        param_file_path = path_config["Paths"]["params_path"]
+        ic_file_path = path_config["Paths"]["ics_path"]
+
+        with open(param_file_path, "r") as file:
+            run_params = yaml.safe_load(file)
+        
+        config = configparser.ConfigParser()
+        config.read_file(open(ic_file_path))
+
+        args = {"h": run_params["Cosmology"]["h"], "Omega_cdm": run_params["Cosmology"]["Omega_cdm"], "Omega_b": run_params["Cosmology"]["Omega_b"]}
+
+        args["engine"] = "class"
+
+        try:
+            args["n_s"] = float(config.get("cosmology", "n_s"))
+        except Exception:
+            pass
+
+        try:
+            args["A_s"] = float(config.get("cosmology", "A_s"))
+        except Exception:
+            pass
+
+        try:
+            args["w0_fld"] = run_params["Cosmology"]["w_0"]
+        except Exception:
+            pass
+
+        try:
+            args["wa_fld"] = run_params["Cosmology"]["w_a"]
+        except Exception:
+            pass
+
+        try:
+            M_nu_eV = run_params["Cosmology"]["M_nu_eV"]
+            M_nu_list = M_nu_eV.split(",")
+            args["N_ncdm"] = [float(i) for i in M_nu_list]
+        except Exception:
+            pass
+
+        cosmo_cosmoprimo = Cosmology_cosmoprimo(**args)
+
+        super().__init__(cosmo_cosmoprimo)
---
+++ abacusnbody/data/flamingo_halo_catalog.py
@@ -0,0 +1,145 @@
+import numpy as np
+import yaml
+import h5py
+import astropy.table
+from astropy.table import Table
+from cosmology import CosmologyFlamingo
+
+# This is a halo catalog loading module designed to 
+# imitate the compaso halo catalog for Flamingo and Peregrinus.
+
+class SwiftHaloCatalog(object):
+    def __init__(self, path_config_filename):
+        """
+        Initiate halo catalogue but don't look at any halos
+
+        Gets self.run_params (dict of the run parameters), self.ic_params (dict of the ics), self.h, self.UnitMass_in_Msol_h, self.halo_type
+        """
+
+        with open(path_config_filename, "r") as file:
+            path_config = yaml.safe_load(file)
+        self.path_config = path_config
+        self.param_file_path = path_config["Paths"]["params_path"]
+        self.ic_file_path = path_config["Paths"]["ics_path"]
+
+        with open(self.param_file_path, "r") as file:
+            self.run_params = yaml.safe_load(file)
+        with open(self.ic_file_path, "r") as file:
+            self.ic_params = yaml.safe_load(file)
+
+        self.h = self.run_params["Cosmology"]["h"]
+        UnitMass_in_cgs = float(self.run_params["InternalUnitSystem"]["UnitMass_in_cgs"])
+        self.UnitMass_in_Msol_h = UnitMass_in_cgs * self.h / 1.98841e33
+
+        self.halo_type = path_config["Misc"]["halo_type"]
+
+        self.cosmology = CosmologyFlamingo(path_config_filename)
+
+    def get_halo_data(self, file_path):
+        if self.halo_type == "soap":
+            self.read_soap_file(file_path)
+        elif self.halo_type == "peregrinus":
+            self.read_peregrinus_file(file_path)
+        else:
+            raise Exception("Only SOAP- or Peregrinus-type halo data supported")
+        
+        zcos = self.path_config["Params"]["redshift"]
+        self.compute_additional_halo_data(zcos)
+    
+    def compute_additional_halo_data(self, zcos):
+        """
+        Gets the 3d velocity dispersion, concentration, and "virial radius" (here just r200 crit)
+
+        Use read_XXXX_file() beforehand to get id, pos, vel, M200_crit
+        """
+        self.halos["R200_crit"] = self.get_r200(zcos)
+        self.halos["concentration"] = self.get_concentration()
+        self.halos["sigmav3d"] = self.get_sigmav3d(zcos)
+
+    def get_sigmav3d(self, zcos):
+        """
+        Get the 3d velocity dispersion: "i.e., the square root of the sum of eigenvalues of the second moment tensor of the velocities relative to the center of mass."
+
+        Uses Eq. 12 of Skibba+2006 (in proper km/s) as presented in Alex Smith's HOD_Mock_Pipeline
+        """
+        return np.sqrt(2.151e-9 * (self.halos["mass"]*\
+                          (1.+zcos)/self.halos["R200_crit"]))
+
+    def get_concentration(self):
+        """
+        Returns NFW concentration of each halo, calculated from
+        R200 and RVmax
+
+        Uses the calculation given by Alex Smith's HOD_Mock_Pipeline
+
+        Returns:
+            array of halo concentrations
+        """
+        conc = 2.16 * self.halos["R200_crit"] / self.halos["rvmax"]
+
+        return np.clip(conc, 0.1, 1e4)
+
+    def get_r200(self, zcos, comoving=True, rho_type="crit"):
+        """
+        Returns R200 of each halo
+
+        Args:
+            comoving: (optional) if True convert to comoving distance
+            rho_type: (optional) "mean" or "crit", default "crit"
+        Returns:
+            array of R200 [Mpc/h]
+        """
+        if rho_type == "crit":
+            rho = self.cosmology.critical_density(zcos)
+            r200 = (3./(800*np.pi) * self.halos["M200_crit"] / rho)**(1./3)
+        elif rho_type == "mean":
+            rho = self.cosmology.mean_density(zcos)
+            raise Exception("Need to implement r200mean first")
+        
+        if comoving:
+            return r200 * (1.+zcos)
+        else:
+            return r200
+    
+    
+    def read_soap_file(self, file_path):
+        halo_cat = h5py.File(file_path, "r")
+
+        is_not_subhalo = np.array(halo_cat["InputHalos"]["HBTplus"]["Depth"]) == 0
+        rvmax_threshold = halo_cat["BoundSubhalo"]["MaximumDarkMatterCircularVelocityRadius"].attrs["Mask Threshold"]
+        is_above_rvmax_threshold = np.array(halo_cat["SO"]["200_crit"]["NumberOfDarkMatterParticles"]) >= rvmax_threshold
+        is_nonzero_rvmax = np.array(halo_cat["BoundSubhalo"]["MaximumDarkMatterCircularVelocityRadius"]) != 0
+
+        relevant_field_halos = np.logical_and(is_above_rvmax_threshold, is_not_subhalo)
+        relevant_field_halos = np.logical_and(relevant_field_halos, is_nonzero_rvmax)
+
+        halos = {}
+        halos["id"] = np.array(halo_cat["VR"]["ID"])[relevant_field_halos]
+        halos["pos"] = np.array(halo_cat["SO"]["200_crit"]["CentreOfMass"])[relevant_field_halos] * self.h
+        halos["vel"] = np.array(halo_cat["SO"]["200_crit"]["CentreOfMassVelocity"])[relevant_field_halos]
+        halos["M200_crit"] = np.array(halo_cat["SO"]["200_crit"]["DarkMatterMass"])[relevant_field_halos] * self.UnitMass_in_Msol_h
+        halos["rvmax"] = np.array(halo_cat["BoundSubhalo"]["MaximumDarkMatterCircularVelocityRadius"])[relevant_field_halos] * self.h
+
+        self.halos = halos
+        #self.halos = Table(halos, copy=False)
+        #self.halos.meta.update(self.header)
+        # TODO: Get other stuff, possibly via Colossus?
+
+    def read_peregrinus_file(self, file_path):
+        halo_cat = h5py.File(file_path, "r")
+        
+        is_not_subhalo = np.array(halo_cat["Subhalos"]["Rank"]) == 0
+        is_not_0mass = np.array(halo_cat["Subhalos"]["BoundM200Crit"]) != 0
+        relevant_field_halos = np.logical_and(is_not_0mass, is_not_subhalo)
+
+        halos = {}
+        halos["id"] = np.array(halo_cat["Subhalos"]['TrackId'])[relevant_field_halos]
+        halos["pos"] = np.array(halo_cat["Subhalos"]["ComovingAveragePosition"])[relevant_field_halos] * self.h
+        halos["vel"] = np.array(halo_cat["Subhalos"]["PhysicalAverageVelocity"])[relevant_field_halos]
+        halos["M200_crit"] = np.array(halo_cat["Subhalos"]["BoundM200Crit"])[relevant_field_halos] * self.UnitMass_in_Msol_h
+        halos["rvmax"] = np.array(halo_cat["Subhalos"]["RmaxComoving"])[relevant_field_halos] * self.h
+
+        self.halos = halos
+        #self.halos = Table(halos, copy=False)
+        #self.halos.meta.update(self.header)
+

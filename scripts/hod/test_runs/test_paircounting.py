74b00baae518bd2cff19e06aea0a95dcb78f526f
Alexander M. <rtg142857@gmail.com>
2025-08-14 12:40:12 +0100

    I forgot the weeeeights

--- scripts/hod/test_runs/test_paircounting.py
+++ scripts/hod/test_runs/test_paircounting.py
@@ -260,8 +260,9 @@ def main(path_config_filename):
             subsample_file = subsample_files[i]
             masked_halos = h5py.File(subsample_file)
             halo_mass = masked_halos["halos"]["M200_crit"]
+            halo_weights = masked_halos["halos"]["hmultis"]
 
-            hmf_big += np.histogram(halo_mass, bins = mass_bins_big)[0]
+            hmf_big += np.histogram(halo_mass, bins = mass_bins_big, weights=halo_weights)[0]
             print("Halo mass function from the files that have been loaded so far:",hmf_big)
 
         print("Total number of halos:", np.sum(hmf_big), flush=True)

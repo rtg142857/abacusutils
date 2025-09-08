70e1feae9bb11e75acc762baca33720738db5522
Alexander M. <rtg142857@gmail.com>
2025-09-08 17:55:19 +0100

    Debugging

--- abacusnbody/hod/fitting/hod_fitting_with_paircounting_stochopy.py
+++ abacusnbody/hod/fitting/hod_fitting_with_paircounting_stochopy.py
@@ -100,4 +100,6 @@ def plot_HODs(save_path, M_h, hod_values, tracers):
         sat = hod_values[tracer+"_sat"]
         plt.loglog(M_h, cen, color=tracer_cols[tracer], label=tracer+" cen")
         plt.loglog(M_h, sat, color=tracer_cols[tracer], linestyle='dashed', label=tracer+" sat")
+        plt.ylim(10**-3, 10**3)
+        plt.legend()
     plt.savefig(save_path)

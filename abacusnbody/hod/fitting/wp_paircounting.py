78f1c57579d44f4073c3d1562821989ce5cdebd1
Alexander M. <rtg142857@gmail.com>
2025-12-01 19:57:01 +0000

    Doublecounting test 1/4

--- abacusnbody/hod/fitting/wp_paircounting.py
+++ abacusnbody/hod/fitting/wp_paircounting.py
@@ -177,10 +177,10 @@ def get_galaxy_pairs(tracer1: str, paircounts: dict, hod_cen1, hod_cen2, hod_sat
         SS = create_weighting_factor(paircounts["satsat_ELGauto"],hod_sat1,hod_sat2) / num_sat_parts**2
         SS1 = create_weighting_factor(paircounts["satsat_onehalo_ELGauto"],hod_sat1,hod_sat2) / ((num_sat_parts*(num_sat_parts-1))/2)
 
-    elif (tracer1 == "ELG" and tracer2 != "ELG") or (tracer2 == "ELG" and tracer1 != "ELG"):
-        CC = create_weighting_factor(paircounts["cencen_ELGcross"],hod_cen1,hod_cen2)
+    elif (tracer1 == "ELG" and tracer2 != "ELG") or (tracer2 == "ELG" and tracer1 != "ELG"): # none of these should be doublecounted
+        CC = create_weighting_factor(paircounts["cencen_ELGcross"],hod_cen1,hod_cen2) * 2 # Doublecounting test 1/4
         CS = create_weighting_factor(paircounts["censat_ELGcross"],hod_cen1,hod_sat2) / num_sat_parts # TODO: CHECK DOUBLECOUNTING!! # Checked; seems to be correct?
-        SS = create_weighting_factor(paircounts["satsat_ELGcross"],hod_sat1,hod_sat2) / num_sat_parts**2
+        SS = create_weighting_factor(paircounts["satsat_ELGcross"],hod_sat1,hod_sat2) / (num_sat_parts**2)
         SS1 = create_weighting_factor(paircounts["satsat_onehalo_ELGcross"],hod_sat1,hod_sat2) / (num_sat_parts**2) # Here too!
 
     else:
@@ -215,10 +215,10 @@ def create_randoms_for_wp(npart, tracer1, r_bin_edges,pi_max,boxsize, tracer2=No
         # calculate the random-random pairs using density * volume
         if tracer2 == tracer1 or tracer2 == None:
             # autocorr
-            rhor = (npart[tracer1]*(npart[tracer1]-1))/global_volume
+            rhor = (npart[tracer1]*(npart[tracer1]-1))/global_volume # these are doublecounted
         else:
             # crosscorr
-            rhor = (npart[tracer1]*npart[tracer2]) /global_volume
+            rhor = (npart[tracer1]*npart[tracer2]) * 2 /global_volume # testing doublecounting
         RR = (dv*rhor)
         #print(RR)
         RR_out[p::pi_max] = RR

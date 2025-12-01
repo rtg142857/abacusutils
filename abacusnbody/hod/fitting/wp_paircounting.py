8f98cca95e6cf6ee63b2a4d57c710708402004d2
Alexander M. <rtg142857@gmail.com>
2025-12-01 21:47:57 +0000

    Test 4/4

--- abacusnbody/hod/fitting/wp_paircounting.py
+++ abacusnbody/hod/fitting/wp_paircounting.py
@@ -180,8 +180,8 @@ def get_galaxy_pairs(tracer1: str, paircounts: dict, hod_cen1, hod_cen2, hod_sat
     elif (tracer1 == "ELG" and tracer2 != "ELG") or (tracer2 == "ELG" and tracer1 != "ELG"): # none of these should be doublecounted
         CC = create_weighting_factor(paircounts["cencen_ELGcross"],hod_cen1,hod_cen2)
         CS = create_weighting_factor(paircounts["censat_ELGcross"],hod_cen1,hod_sat2) / num_sat_parts # TODO: CHECK DOUBLECOUNTING!! # Checked; seems to be correct?
-        SS = create_weighting_factor(paircounts["satsat_ELGcross"],hod_sat1,hod_sat2) * 2 / (num_sat_parts**2) # Doublecounting test 2/4
-        SS1 = create_weighting_factor(paircounts["satsat_onehalo_ELGcross"],hod_sat1,hod_sat2) / (num_sat_parts**2) # Here too!
+        SS = create_weighting_factor(paircounts["satsat_ELGcross"],hod_sat1,hod_sat2) / (num_sat_parts**2) 
+        SS1 = create_weighting_factor(paircounts["satsat_onehalo_ELGcross"],hod_sat1,hod_sat2) * 2 / (num_sat_parts**2) # doublecounting test 4/4
 
     else:
         CC = create_weighting_factor(paircounts["cencen"],hod_cen1,hod_cen2)

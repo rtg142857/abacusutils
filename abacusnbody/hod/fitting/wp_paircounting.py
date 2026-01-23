90ee72debe2671d932617b4c079751d239d9c129
Alexander M. <rtg142857@gmail.com>
2026-01-23 12:15:52 +0000

    testing ELGs without ELG centrals

--- abacusnbody/hod/fitting/wp_paircounting.py
+++ abacusnbody/hod/fitting/wp_paircounting.py
@@ -35,7 +35,7 @@ def N_cen_ELG_v1(M_h: np.ndarray, p_max, Q, logM_cut, sigma, gamma, Anorm=1):
     Phi = Phi_fun(logM_h, logM_cut, sigma, gamma)
     return (
         2.0 * (p_max - 1.0 / Q) * phi * Phi / Anorm
-    ) * 0 + 0.5 * erfc((logM_cut - np.log10(M_h)) / (1.41421356 * sigma)) # FOR DEBUGGING; TODO: UNDO
+    ) * 0 + 0.5 * erfc((logM_cut - np.log10(M_h)) / (1.41421356 * sigma)) * 0 # FOR DEBUGGING; TODO: UNDO
     # + 0.5/Q*(1 + math.erf((logM_h-logM_cut-0.8)*3))
 
 def N_sat_ELG(M_h, logM_cut, kappa, logM_1, alpha, A_s=1.0, alpha1=0.0, beta=0.0):

e1dd9053546e8422c0a8dec9e03a0f991c356ee2
Alexander M. <rtg142857@gmail.com>
2026-02-06 12:13:09 +0000

    can't seed default RNG in njit

--- abacusnbody/hod/GRAND_HOD.py
+++ abacusnbody/hod/GRAND_HOD.py
@@ -608,7 +608,7 @@ def compute_fast_NFW(
     return h_id, x_sat, y_sat, z_sat, vx_sat, vy_sat, vz_sat, M, Rvir
 
 
-@njit(fastmath=True, parallel=True)
+#@njit(fastmath=True, parallel=True)
 def gen_sats_nfw(
     NFW_draw,
     hpos,

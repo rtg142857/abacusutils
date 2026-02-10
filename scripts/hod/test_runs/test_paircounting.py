5c6b563dedfc4e7bac435007d1f1de4516cb2765
Alexander M. <rtg142857@gmail.com>
2026-02-10 15:59:01 +0000

    Nvm there was a timeout

--- scripts/hod/test_runs/test_paircounting.py
+++ scripts/hod/test_runs/test_paircounting.py
@@ -152,7 +152,7 @@ def main(path_config_filename):
     if True: # not mock_wp_exists:
         print("Getting true mock to compare wp against", flush=True)#############################################################
         mock_dict = newBall.run_hod(
-            newBall.tracers, want_rsd, want_nfw=True, NFW_draw=NFW_draw, write_to_disk=True, Nthread=32, verbose=True, tabulation_mock=False, seed=seed
+            newBall.tracers, want_rsd, want_nfw=True, NFW_draw=NFW_draw, write_to_disk=False, Nthread=32, verbose=True, tabulation_mock=False, seed=seed
         )
         np.save(temp_stuff + "internal_hmf.npy", newBall.halo_mass_func)
         print("Calculating number of galaxies from the in-built function now", flush=True)

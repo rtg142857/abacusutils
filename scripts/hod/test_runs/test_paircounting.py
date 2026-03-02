6999e8bffa700bf3a736e1cf2a6362e3172eb763
Alexander M. <rtg142857@gmail.com>
2026-03-02 17:00:31 +0000

    TODO: Revert

--- scripts/hod/test_runs/test_paircounting.py
+++ scripts/hod/test_runs/test_paircounting.py
@@ -165,12 +165,12 @@ def main(path_config_filename):
         print("Number of LRGs according to compute_ngal:", ngal_dict)
 
         print("Getting wp from the true mock", flush=True)#############################################################
-        mock_dict_sat = {}
-        for tracer in tracer_list:
-            Ncent = mock_dict[tracer]["Ncent"]
-            mock_dict_sat[tracer] = {}
-            for field in ["x", "y", "z", "vx", "vy", "vz", "mass", "id"]:
-                mock_dict_sat[tracer][field] = mock_dict[tracer][field][Ncent:]
+        # mock_dict_sat = {}
+        # for tracer in tracer_list:
+        #     Ncent = mock_dict[tracer]["Ncent"]
+        #     mock_dict_sat[tracer] = {}
+        #     for field in ["x", "y", "z", "vx", "vy", "vz", "mass", "id"]:
+        #         mock_dict_sat[tracer][field] = mock_dict[tracer][field][Ncent:]
         wp_dict = newBall.compute_xirppi(mock_dict, rpbins, pimax, pi_bin_size, Nthread=32) # TODO: Revert
         print("wp_dict with cens: ",wp_dict)
         # print("Now for the satelllites only:")

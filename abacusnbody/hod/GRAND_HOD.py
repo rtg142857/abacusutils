8c385de19bac64de9ee35ed058dadda7d666c923
Alexander M. <rtg142857@gmail.com>
2026-03-20 17:40:26 +0000

    ??

--- abacusnbody/hod/GRAND_HOD.py
+++ abacusnbody/hod/GRAND_HOD.py
@@ -606,15 +606,16 @@ def compute_fast_NFW(
     x_h = Lbox/100 * np.arange(100).repeat(10000).reshape((-1, 10000)).flatten("F") # np.tile(np.array(range(100)), 10000) # [0, 1, 2, ..., 0, 1, 2, ..., 0, 1, 2...]
     y_h = Lbox/100 * np.arange(100).repeat(100).repeat(100).reshape((-1, 100)).T.flatten() # np.tile(np.repeat(np.array(range(100)), 100), 100)
     z_h = Lbox/100 * np.repeat(np.arange(100), 10000)
-    # x_h = np.concatenate((x_h, np.zeros(len(M)-len(x_h))))
-    # y_h = np.concatenate((y_h, np.zeros(len(M)-len(y_h))))
-    # z_h = np.concatenate((z_h, np.zeros(len(M)-len(z_h))))
-    print(len(M))
-    print(len(x_h))
-    print(np.shape(rd_pos))
-    x_h = x_h[:len(M)]
-    y_h = y_h[:len(M)]
-    z_h = z_h[:len(M)]
+    padright = np.zeros(len(h_id) - len(x_h))
+    x_h = np.concatenate((x_h, padright))
+    y_h = np.concatenate((y_h, padright))
+    z_h = np.concatenate((z_h, padright))
+    # print(len(M))
+    # print(len(x_h))
+    # print(np.shape(rd_pos))
+    # x_h = x_h[:len(M)]
+    # y_h = y_h[:len(M)]
+    # z_h = z_h[:len(M)]
 
     # starting index of each thread
     hstart = np.rint(np.linspace(0, num_sat.sum(), Nthread + 1))

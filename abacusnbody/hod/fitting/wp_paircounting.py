9c1648807fa337649ed22581b44d85b328654250
Alexander M. <rtg142857@gmail.com>
2026-04-13 17:15:11 +0100

    Going back to the default HOD to check how deep it goes

--- abacusnbody/hod/fitting/wp_paircounting.py
+++ abacusnbody/hod/fitting/wp_paircounting.py
@@ -71,16 +71,16 @@ def N_cen_QSO(M_h, logM_cut, sigma):
     """
     HOD function (Zheng et al. (2005) with p_max) for QSO centrals taken from arXiv:2007.09012.
     """
-    mass_bin_edges = 10**10 * np.logspace(0,6,31)
-    mass_lower = mass_bin_edges[5]
-    mass_upper = mass_bin_edges[6]
-    below_cut = M_h - mass_lower < 0
-    above_cut = M_h - mass_upper > 0
-    hod_value = np.ones(np.size(M_h))
-    hod_value[below_cut] = 0
-    hod_value[above_cut] = 0
-    return hod_value
-    # return 0.5 * (1 + erf((np.log10(M_h) - logM_cut) / 1.41421356 / sigma))
+    # mass_bin_edges = 10**10 * np.logspace(0,6,31)
+    # mass_lower = mass_bin_edges[5]
+    # mass_upper = mass_bin_edges[6]
+    # below_cut = M_h - mass_lower < 0
+    # above_cut = M_h - mass_upper > 0
+    # hod_value = np.ones(np.size(M_h))
+    # hod_value[below_cut] = 0
+    # hod_value[above_cut] = 0
+    # return hod_value
+    return 0.5 * (1 + erf((np.log10(M_h) - logM_cut) / 1.41421356 / sigma))
 
 def N_sat_QSO(M_h, logM_cut, kappa, logM_1, alpha, A_s=1.0):
     """

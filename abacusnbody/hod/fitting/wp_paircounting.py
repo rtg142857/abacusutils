8d4b22b5cc4aa22417f29d7d28eddcc147505d89
Alexander M. <rtg142857@gmail.com>
2026-04-16 17:55:03 +0100

    Binary searching

--- abacusnbody/hod/fitting/wp_paircounting.py
+++ abacusnbody/hod/fitting/wp_paircounting.py
@@ -72,7 +72,7 @@ def N_cen_QSO(M_h, logM_cut, sigma):
     HOD function (Zheng et al. (2005) with p_max) for QSO centrals taken from arXiv:2007.09012.
     """
     mass_bin_edges = 10**10 * np.logspace(0,6,31)
-    mass_lower = mass_bin_edges[10]
+    mass_lower = mass_bin_edges[20]
     mass_upper = mass_bin_edges[30]
     below_cut = M_h - mass_lower < 0
     above_cut = M_h - mass_upper > 0

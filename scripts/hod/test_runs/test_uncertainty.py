184a3108bbe498afaa6db3067c7e31a8a9f40b57
Alexander M <rtg142857@gmail.com>
2026-04-22 17:30:39 +0100

    For merging

--- scripts/hod/test_runs/test_uncertainty.py
+++ scripts/hod/test_runs/test_uncertainty.py
@@ -29,9 +29,9 @@ def main(path_config_filename):
     boxsize = config["Params"]["L"] * run_params["Cosmology"]["h"]
 
     ### Paths ###
-    save_path = "/cosma8/data/dp004/dc-mene1/abacusutils/scripts/hod/samplers/"
+    save_path = "/cosma8/data/dp004/dc-mene1/abacusutils/scripts/hod/"
     paircount_path = "/cosma8/data/dp004/dc-mene1/abacusutils/scripts/hod/output/paircounts/" + sim_label + "/"
-    target_dict_path = config["fitting_params"]["target_dict_path"]
+    target_dict_path = "/cosma8/data/dp004/dc-mene1/abacusutils/scripts/hod/fitting_data/Y1_z0.8-1.1/"
 
     print("Loading precomputed things...")
     paircounts = {}

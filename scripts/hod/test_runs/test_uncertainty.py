c6626af5c99726cbcdab290794ec30ccd1267062
Alexander M. <rtg142857@gmail.com>
2026-05-12 12:51:42 +0100

    Checking variance by hand

--- scripts/hod/test_runs/test_uncertainty.py
+++ scripts/hod/test_runs/test_uncertainty.py
@@ -2,17 +2,44 @@ import numpy as np
 import yaml
 import argparse
 from abacusnbody.hod.fitting.hod_fitting_with_paircounting_stochopy import make_other_stuff_dict
-from abacusnbody.hod.fitting.setup_paircounting_fitting import get_npart, get_wp
+from abacusnbody.hod.fitting.setup_paircounting_fitting import get_npart, get_wp, log_probability, get_target_number_density
 from pycorr import TwoPointCorrelationFunction, twopoint_estimator
 
-def get_target_dicts(target_dict_path, tracers=["LRG", "ELG", "QSO"]):
+# def get_target_dicts(target_dict_path, tracers=["LRG", "ELG", "QSO"]):
+#     """
+#     Returns dictionaries indexed by "LRG_LRG", "LRG_ELG", etc.
+#     One for the wp, one for the jackknife.
+#     """
+#     wp_dict = {}
+#     inverse_jackknife_dict = {}
+#     jackknife_dict = {}
+#     for i1, tr1 in enumerate(tracers):
+#         for i2, tr2 in enumerate(tracers):
+#             if i1 <= i2:
+#                 #crosscorr or autocorr
+#                 # path = target_dict_path + tr1 + "_" + tr2 + ".txt"
+#                 # rpmid, rpavg, corr, std = np.loadtxt(path, unpack=True)
+#                 # wp_dict[tr1+"_"+tr2] = corr
+#                 # jackknife_dict[tr1+"_"+tr2] = std
+
+#                 path = target_dict_path + tr1 + "_" + tr2 + ".npy"
+#                 estimator = TwoPointCorrelationFunction.load(path)
+#                 # rebinned_estimator = estimator[:(estimator.shape[0] // 2) * 2:2] # getting it to be 24 bins
+#                 sep, wp, cov = twopoint_estimator.project_to_wp(estimator, return_cov=True)
+#                 cov_inv = np.linalg.inv(cov)
+#                 wp_dict[tr1+"_"+tr2] = wp
+#                 inverse_jackknife_dict[tr1+"_"+tr2] = cov_inv
+#                 jackknife_dict[tr1+"_"+tr2] = cov
+#     return wp_dict, inverse_jackknife_dict, jackknife_dict
+
+def get_target_dicts(target_dict_path, tracers=["LRG", "ELG", "QSO"], wp_limit=(0, 24)):
     """
     Returns dictionaries indexed by "LRG_LRG", "LRG_ELG", etc.
     One for the wp, one for the jackknife.
     """
+    lb, ub=wp_limit
     wp_dict = {}
     inverse_jackknife_dict = {}
-    jackknife_dict = {}
     for i1, tr1 in enumerate(tracers):
         for i2, tr2 in enumerate(tracers):
             if i1 <= i2:
@@ -24,15 +51,19 @@ def get_target_dicts(target_dict_path, tracers=["LRG", "ELG", "QSO"]):
 
                 path = target_dict_path + tr1 + "_" + tr2 + ".npy"
                 estimator = TwoPointCorrelationFunction.load(path)
-                rebinned_estimator = estimator[:(estimator.shape[0] // 2) * 2:2] # getting it to be 24 bins
-                sep, wp, cov = twopoint_estimator.project_to_wp(rebinned_estimator, return_cov=True)
+                # if estimator.shape[0] != 24:
+                #     estimator = estimator[:(estimator.shape[0] // 2) * 2:2] # getting it to be 24 bins
+                sep, wp, cov = twopoint_estimator.project_to_wp(estimator, return_cov=True)
+                wp = wp[lb:ub]
+                cov = cov[lb:ub, lb:ub]
+                ### FOR DEBUGGING; TODO: UNDO
+                # cov = np.diag(np.diag(cov)) # Making the array diagonal-only, so no covariance between points
                 cov_inv = np.linalg.inv(cov)
                 wp_dict[tr1+"_"+tr2] = wp
                 inverse_jackknife_dict[tr1+"_"+tr2] = cov_inv
-                jackknife_dict[tr1+"_"+tr2] = cov
-    return wp_dict, inverse_jackknife_dict, jackknife_dict
+    return wp_dict, inverse_jackknife_dict
 
-def plot_wp(save_path, hod_params, tracers, paircounts, target_wp, target_jackknife, other_stuff_dict_here, clustering_params):
+def plot_wp(save_path, hod_params, tracers, paircounts, target_wp, target_jackknife_inverse, other_stuff_dict_here, clustering_params, wp_limit=(0, 24)):
     import matplotlib.pyplot as plt
 
     npart = get_npart(hod_params, tracers, other_stuff_dict_here)
@@ -44,8 +75,9 @@ def plot_wp(save_path, hod_params, tracers, paircounts, target_wp, target_jackkn
 
     label_dict = {"0_0": "LRG_LRG", "0_1": "ELG_ELG", "0_2": "QSO_QSO", "1_0": "LRG_ELG", "1_1": "LRG_QSO", "1_2": "ELG_QSO"}
 
-    i0 = 5 # 
-    i1 = np.size(target_wp["LRG_LRG"])
+    # i0 = 5 # 
+    # i1 = np.size(target_wp["LRG_LRG"])
+    i0, i1 = wp_limit
 
     fig, axs = plt.subplots(2, 3, figsize=(15, 8))
     for y in range(2):
@@ -54,11 +86,11 @@ def plot_wp(save_path, hod_params, tracers, paircounts, target_wp, target_jackkn
             rwp_flamingo = rpcent * wp_dict[label_dict[yx_label]]
             axs[y, x].plot(rpcent, rwp_flamingo, "-b", label="FlamingoHOD")
 
-            rwp_data = rpcent * target_wp[label_dict[yx_label]]
-            rwp_error_mat = target_jackknife[label_dict[yx_label]]
-            rwp_error = np.sqrt(np.diagonal(rwp_error_mat)) * rpcent
-            axs[y, x].errorbar(rpcent[i0:i1], rwp_data[i0:i1], yerr=rwp_error[i0:i1], color="orange", label=f"Data ("+label_dict[yx_label]+")")
-            axs[y, x].plot(rpcent[:i0], rwp_data[:i0], marker="o", color="black", label="Data (unused)")
+            rwp_data = rpcent[i0:i1] * target_wp[label_dict[yx_label]]
+            rwp_error_mat = np.linalg.inv(target_jackknife_inverse[label_dict[yx_label]])
+            rwp_error = np.sqrt(np.diagonal(rwp_error_mat)) * rpcent[i0:i1]
+            axs[y, x].errorbar(rpcent[i0:i1], rwp_data, yerr=rwp_error, color="orange", label=f"Data ("+label_dict[yx_label]+")")
+            #axs[y, x].plot(rpcent[:i0], rwp_data[:i0], marker="o", color="black", label="Data (unused)")
             axs[y, x].set_xscale('log')
             axs[y, x].legend()
 
@@ -103,16 +135,22 @@ def main(path_config_filename):
         for pair_type in ["", "_ELGauto", "_ELGcross"]:
             filename = paircount_path + pair + pair_type + ".npy"
             paircounts[pair+pair_type] = np.load(filename)
-    target_wp, target_jackknife_inverse, target_jackknife = get_target_dicts(target_dict_path, tracers=tracer_list)
+    target_wp, target_jackknife_inverse, target_jackknife = get_target_dicts(target_dict_path, tracers=tracer_list, wp_limit=(3,18))
     print(target_wp)
     for key in target_jackknife.keys():
         print(key+":")
         print(np.diag(target_jackknife_inverse[key]))
         print(np.diag(target_jackknife[key]))
     other_stuff_dict_here = make_other_stuff_dict(boxsize=boxsize, num_sat_parts=3, subsample_dir=subsample_dir, sim_label=sim_label)
+    print("Getting chi squared:")
+    wp_limit = (3, 18)
+    target_ngal = get_target_number_density(tracers=tracer_list)
+    clustering_params = config["clustering_params"]
+    minimum=True
+    logprob = log_probability(paircounts, tracer_list, target_wp, target_jackknife_inverse, target_ngal, other_stuff_dict_here, clustering_params, minimum, wp_limit)
     print("Plotting wps...", flush=True)
     plot_wp(save_path+"wps.png", hod_params=HOD_params_list, tracers=tracer_list, paircounts=paircounts,
-            target_wp=target_wp, target_jackknife=target_jackknife, other_stuff_dict_here=other_stuff_dict_here, clustering_params=clustering_params)
+            target_wp=target_wp, target_jackknife=target_jackknife, other_stuff_dict_here=other_stuff_dict_here, clustering_params=clustering_params, wp_limit=(3,18))
 
 class ArgParseFormatter(
     argparse.RawDescriptionHelpFormatter, argparse.ArgumentDefaultsHelpFormatter

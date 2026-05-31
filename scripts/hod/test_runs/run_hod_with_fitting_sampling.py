dd2de09029a4e0ae92e27ff1222f0f9a0c30e131
Alexander M. <rtg142857@gmail.com>
2026-05-31 02:34:57 +0100

    Test runs for the hmc

--- scripts/hod/test_runs/run_hod_with_fitting_sampling.py
+++ scripts/hod/test_runs/run_hod_with_fitting_sampling.py
@@ -16,7 +16,7 @@ import yaml
 
 from abacusnbody.hod.flamingo_hod import FlamingoHOD
 from abacusnbody.hod.NFW import nfw_draw
-from abacusnbody.hod.fitting.hod_fitting_with_paircounting_emcee import fit_HOD
+from abacusnbody.hod.fitting.hod_fitting_with_paircounting_mcmc import fit_HOD
 from abacusnbody.hod.fitting.paircounting import get_paircounts
 
 DEFAULTS = {}

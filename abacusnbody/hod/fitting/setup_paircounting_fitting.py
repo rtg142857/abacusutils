1fcf61e0414f3b5713830c98c81515e2d36583ce
Alexander M. <rtg142857@gmail.com>
2026-07-22 12:35:29 +0100

    Trying more lenient sigma n for ng, because things aren't converging properly

--- abacusnbody/hod/fitting/setup_paircounting_fitting.py
+++ abacusnbody/hod/fitting/setup_paircounting_fitting.py
@@ -80,12 +80,12 @@ def negative_chi_squared_ng_single_tracer(fitting_ngal, target_ngal, tracer):
     """
     Using the "rather lenient" sigma_n in Eq. 18 in https://arxiv.org/pdf/2110.11412 results in the wp dominating the chi squared
     Which is bad because it wants to push the incompleteness above 100%
-    So we drop sigma_n by a factor of 100
+    So we drop sigma_n by a factor of 100 (10?)
 
     Assume QSOs are complete
     """
     if fitting_ngal < target_ngal or tracer == "QSO":
-        sigma_n = 4 * 10 ** (-7) # 4 * 10 ** (-5)
+        sigma_n = 4 * 10 ** (-6) #4 * 10 ** (-7) # 4 * 10 ** (-5)
         return -((fitting_ngal - target_ngal) / sigma_n) **2
     else:
         return 0

684138628582c8cd76690688ab53cc1dc687e90d
Alexander M. <rtg142857@gmail.com>
2026-07-27 14:36:33 +0100

    Debugging

--- scripts/hod/test_runs/test_paircounting.py
+++ scripts/hod/test_runs/test_paircounting.py
@@ -258,7 +258,7 @@ def main(path_config_filename):
 
     print("Plotting HODs...")
     M_h = np.logspace(10, 16, 90)
-    hod_values = get_hod_values_given_parameters_with_incompleteness(M_h, HOD_params_list, tracer_list, other_stuff_dict_here)
+    hod_values = get_hod_values_given_parameters_with_incompleteness(M_h, HOD_params_list, param_set, other_stuff_dict_here)
     print(hod_values)
     plot_HODs("HODs.png", M_h, hod_values, tracer_list)
 

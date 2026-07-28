c4307db258a3670b901237eaa929095da5f3d272
Alexander M. <rtg142857@gmail.com>
2026-07-28 11:37:50 +0100

    Print-statement debugging

--- abacusnbody/hod/fitting/params.py
+++ abacusnbody/hod/fitting/params.py
@@ -297,8 +297,12 @@ class Params:
         Returns a tuple: the ordinary average, and the value for ELG-ELG one-halo.
         """
         assert tracer=="ELG"
+        print("Debugging: Finding conformity weighted sat HODs")
         ELG_hod_cen, ELG_hod_sat_noconform = self.get_hods_given_tracer_and_params(M_h, hod_params, tracer="ELG", ELG_ELG=False)
+        print(f"ELG HOD cen:\n{ELG_hod_cen}")
+        print(f"ELG HOD sat baseline:\n{ELG_hod_sat_noconform}")
         _, ELG_hod_sat_conform = self.get_hods_given_tracer_and_params(M_h, hod_params, tracer="ELG", ELG_ELG=True)
+        print(f"ELG HOD sat with conformity (SHOULD BE THE SAME AS BASELINE):\n{ELG_hod_sat_conform}")
 
         QSO_params = self.get_params_of_specific_tracer(hod_params, "QSO")
         LRG_hod_cen_unweighted, _ = self.get_hods_given_tracer_and_params(M_h, hod_params, tracer="LRG")
@@ -306,11 +310,15 @@ class Params:
         QSO_hod_cen, _ = self.get_hods_given_tracer_and_params(M_h, hod_params, tracer="QSO")
         
         nonELG_hod_cen = LRG_hod_cen + QSO_hod_cen
+        print(f"Sum of non-ELG hod cens (SHOULD BE 1.0 at high end):\n{nonELG_hod_cen}")
         ELG_cen_ratio = np.nan_to_num(ELG_hod_cen / (nonELG_hod_cen + ELG_hod_cen))
+        print(f"ELG cen ratio:\n{ELG_cen_ratio}")
         ELG_hod_sat_avg = ELG_hod_sat_conform * ELG_cen_ratio + ELG_hod_sat_noconform * (1 - ELG_cen_ratio)
+        print(f"Cen-weighted conformity ELG sat HOD (should be the same as ELG sat HOD):\n{ELG_hod_sat_avg}")
 
         # derivation is complicated; average of X(X-1), where X is a linear combination of Poisson distributions
         ELG_hod_sat_ss1_squared = ELG_hod_sat_avg**2 - ELG_hod_sat_avg + ELG_hod_sat_conform * ELG_cen_ratio**2 + ELG_hod_sat_noconform * (1-ELG_cen_ratio)**2
+        print(f"ELG-ELG 1-halo satsat HOD (should be the same as ELG sat HOD):\n{np.sqrt(ELG_hod_sat_ss1_squared)}")
         return ELG_hod_sat_avg, np.sqrt(ELG_hod_sat_ss1_squared)
     
     # def update_hods(self, hod_params):

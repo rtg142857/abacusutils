eaad7d1f642f1e93322094dd50ac789c5aff59ea
Alexander M. <rtg142857@gmail.com>
2026-08-03 14:36:57 +0100

    Only ELGs

--- abacusnbody/hod/fitting/params.py
+++ abacusnbody/hod/fitting/params.py
@@ -13,7 +13,7 @@ def N_cen_LRG(M_h: np.ndarray, logM_cut, sigma):
     """
     Standard Zheng et al. (2005) central HOD parametrization for LRGs.
     """
-    return 0.5 * erfc((logM_cut - np.log10(M_h)) / (1.41421356 * sigma))
+    return 0.5 * erfc((logM_cut - np.log10(M_h)) / (1.41421356 * sigma)) * 0 # for debugging; TODO: UNDO
 
 def N_sat_LRG_modified(M_h: np.ndarray, logM_cut, logM_1, sigma, alpha, kappa):
     """

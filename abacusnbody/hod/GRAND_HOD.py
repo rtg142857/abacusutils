c5430e8af571fb8a3bcfe9ec62cb66fc5f86dfeb
Alexander M. <rtg142857@gmail.com>
2026-08-10 18:16:14 +0100

    Back to the original HODs, having both conformity and nonconformity sats be in play

--- abacusnbody/hod/GRAND_HOD.py
+++ abacusnbody/hod/GRAND_HOD.py
@@ -61,7 +61,7 @@ def n_cen_LRG(M_h, logM_cut, sigma):
     """
     Standard Zheng et al. (2005) central HOD parametrization for LRGs.
     """
-    return 0.5 * math.erfc((logM_cut - np.log10(M_h)) / (1.41421356 * sigma)) * 0 # for debugging; TODO: UNDO
+    return 0.5 * math.erfc((logM_cut - np.log10(M_h)) / (1.41421356 * sigma))# * 0 # for debugging; TODO: UNDO
 
 
 @njit(fastmath=True)
@@ -109,7 +109,7 @@ def N_cen_ELG_v1(M_h, p_max, Q, logM_cut, sigma, gamma, Anorm=1):
     logM_h = np.log10(M_h)
     phi = phi_fun(logM_h, logM_cut, sigma)
     Phi = Phi_fun(logM_h, logM_cut, sigma, gamma)
-    return 0.5 * math.erfc((logM_cut - np.log10(M_h)) / (1.41421356 * sigma)) # For debugging; TODO: UNDO
+    #return 0.5 * math.erfc((logM_cut - np.log10(M_h)) / (1.41421356 * sigma)) # For debugging; TODO: UNDO
     return (
         2.0 * (p_max - 1.0 / Q) * phi * Phi / Anorm
     ) + 0.5/Q*(1 + math.erf((logM_h-logM_cut)/0.01)) # + 0.5/Q*(1 + math.erf((logM_h-logM_cut-0.8)*3))
@@ -141,7 +141,7 @@ def N_cen_QSO(M_h, logM_cut, sigma, p_max):
     #     return 0
     # else:
     #     return 1
-    return p_max * 0.5 * (1 + math.erf((np.log10(M_h) - logM_cut) / 1.41421356 / sigma)) * 0 # For debugging; TODO: UNDO
+    return p_max * 0.5 * (1 + math.erf((np.log10(M_h) - logM_cut) / 1.41421356 / sigma))# * 0 # For debugging; TODO: UNDO
 
 
 @njit(fastmath=True)
--- abacusnbody/hod/fitting/params.py
+++ abacusnbody/hod/fitting/params.py
@@ -13,7 +13,7 @@ def N_cen_LRG(M_h: np.ndarray, logM_cut, sigma):
     """
     Standard Zheng et al. (2005) central HOD parametrization for LRGs.
     """
-    return 0.5 * erfc((logM_cut - np.log10(M_h)) / (1.41421356 * sigma)) * 0 # for debugging; TODO: UNDO
+    return 0.5 * erfc((logM_cut - np.log10(M_h)) / (1.41421356 * sigma))# * 0 # for debugging; TODO: UNDO
 
 def N_sat_LRG_modified(M_h: np.ndarray, logM_cut, logM_1, sigma, alpha, kappa):
     """
@@ -45,7 +45,7 @@ def N_cen_ELG_v1(M_h: np.ndarray, p_max, Q, logM_cut, sigma, gamma, Anorm=1):
     logM_h = np.log10(M_h)
     phi = phi_fun(logM_h, logM_cut, sigma)
     Phi = Phi_fun(logM_h, logM_cut, sigma, gamma)
-    return 0.5 * erfc((logM_cut - np.log10(M_h)) / (1.41421356 * sigma)) # for debugging; TODO: UNDO
+    #return 0.5 * erfc((logM_cut - np.log10(M_h)) / (1.41421356 * sigma)) # for debugging; TODO: UNDO
     return ((
         2.0 * (p_max - 1.0 / Q) * phi * Phi / Anorm
     ) + 0.5/Q*(1 + erf((logM_h-logM_cut)/0.01)))
@@ -85,7 +85,7 @@ def N_cen_QSO(M_h, logM_cut, sigma, p_max):
     # hod_value[below_cut] = 0
     # hod_value[above_cut] = 0
     # return hod_value
-    return p_max * 0.5 * (1 + erf((np.log10(M_h) - logM_cut) / 1.41421356 / sigma)) * 0 # For debugging; TODO: UNDO
+    return p_max * 0.5 * (1 + erf((np.log10(M_h) - logM_cut) / 1.41421356 / sigma))# * 0 # For debugging; TODO: UNDO
 
 def N_sat_QSO(M_h, logM_cut, kappa, logM_1, alpha, A_s=1.0):
     """

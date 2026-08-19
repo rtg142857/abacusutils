990e28e94c0a918b3b742e4f9b71bc7eea7c8d63
Alexander M. <rtg142857@gmail.com>
2026-08-19 16:55:08 +0100

    Resetting the HMF to normal

--- abacusnbody/hod/GRAND_HOD.py
+++ abacusnbody/hod/GRAND_HOD.py
@@ -1797,9 +1797,9 @@ def gen_gals(
     for key in halos_array.keys():
         if hasattr(halos_array[key], '__len__'):
             halos_array[key] = halos_array[key][:10**6]
-    halos_array['hmass'] = np.full(shape=len(halos_array['hmass']), fill_value=10**12.5)
-    halos_array['hrvir'] = np.full(shape=len(halos_array['hrvir']), fill_value=10**1.0)
-    halos_array['hmultis'] = np.full(shape=len(halos_array['hmultis']), fill_value=1.0)
+    #halos_array['hmass'] = np.full(shape=len(halos_array['hmass']), fill_value=10**12.5)
+    #halos_array['hrvir'] = np.full(shape=len(halos_array['hrvir']), fill_value=10**1.0)
+    #halos_array['hmultis'] = np.full(shape=len(halos_array['hmultis']), fill_value=1.0)
 
     LRG_dict_cent, ELG_dict_cent, QSO_dict_cent, ID_dict_cent, keep_cent, hrvir_dict_cent = gen_cent(
         halos_array['hpos'],
--- abacusnbody/hod/fitting/setup_paircounting_fitting.py
+++ abacusnbody/hod/fitting/setup_paircounting_fitting.py
@@ -248,9 +248,9 @@ def make_other_stuff_dict(boxsize, num_sat_parts, subsample_dir, sim_label):
         masked_halos = h5py.File(subsample_file)
         halo_mass = masked_halos["halos"]["M200_crit"]
         # For debugging; TODO: UNDO
-        halo_mass = np.full(shape=10**6, fill_value=10**12.5)
+        #halo_mass = np.full(shape=10**6, fill_value=10**12.5)
         halo_weights = masked_halos["halos"]["multi_halos"]
-        halo_weights = np.full(shape=10**6, fill_value=1.0)
+        #halo_weights = np.full(shape=10**6, fill_value=1.0)
 
         hmf_big += np.histogram(halo_mass, bins = mass_bins_big, weights=halo_weights)[0]
         #print("Halo mass function from the files that have been loaded so far:",hmf_big)

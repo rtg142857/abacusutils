672fd45fd704b0b91f452e8214568b50a043c982
Alexander M. <rtg142857@gmail.com>
2026-08-25 10:26:51 +0100

    oops

--- abacusnbody/hod/fitting/params.py
+++ abacusnbody/hod/fitting/params.py
@@ -97,7 +97,7 @@ def N_cen_QSO(M_h, logM_cut, sigma, p_max):
     # hod_value = np.full(shape=len(M_h), fill_value=SMALL_NUMBER)
     # hod_value[below_cut] = 0
     # return hod_value
-    return p_max * 0.5 * (1 + erf((np.log10(M_h) - logM_cut) / 1.41421356 / sigma)) # * 0 # For debugging; TODO: UNDO
+    return p_max * 0.5 * (1 + erf((np.log10(M_h) - logM_cut) / 1.41421356 / sigma)) * SMALL_NUMBER # * 0 # For debugging; TODO: UNDO
     #return np.full(len(M_h), fill_value=SMALL_NUMBER * p_max)
 
 def N_sat_QSO(M_h, logM_cut, kappa, logM_1, alpha, A_s=1.0):

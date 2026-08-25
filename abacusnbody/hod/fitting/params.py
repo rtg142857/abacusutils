8b1e3cb539a25130c6ea49d69bc8c89286d3be3f
Alexander M. <rtg142857@gmail.com>
2026-08-25 09:45:28 +0100

    Resetting to ordinary HODs

--- abacusnbody/hod/fitting/params.py
+++ abacusnbody/hod/fitting/params.py
@@ -49,15 +49,15 @@ def N_cen_ELG_v1(M_h: np.ndarray, p_max, Q, logM_cut, sigma, gamma, Anorm=1):
     logM_h = np.log10(M_h)
     phi = phi_fun(logM_h, logM_cut, sigma)
     Phi = Phi_fun(logM_h, logM_cut, sigma, gamma)
-    #return 0.5 * erfc((logM_cut - np.log10(M_h)) / (1.41421356 * sigma)) # for debugging; TODO: UNDO
-    #return np.full(len(M_h), fill_value=SMALL_NUMBER * p_max)
-    # return ((
-    #     2.0 * (p_max - 1.0 / Q) * phi * Phi / Anorm
-    # ) + 0.5/Q*(1 + erf((logM_h-logM_cut)/0.01))) * SMALL_NUMBER
-    below_cut = M_h < MASS_CUTOFF
-    hod_value = np.full(shape=len(M_h), fill_value=SMALL_NUMBER)
-    hod_value[~below_cut] = 0
-    return hod_value
+    #return 0.5 * erfc((logM_cut - np.log10(M_h)) / (1.41421356 * sigma)) # LRG cen function
+    #return np.full(len(M_h), fill_value=SMALL_NUMBER * p_max) # constant function
+    return (( # normal one (watch the SMALL_NUMBER)
+        2.0 * (p_max - 1.0 / Q) * phi * Phi / Anorm
+    ) + 0.5/Q*(1 + erf((logM_h-logM_cut)/0.01))) * SMALL_NUMBER
+    # below_cut = M_h < MASS_CUTOFF # step function
+    # hod_value = np.full(shape=len(M_h), fill_value=SMALL_NUMBER)
+    # hod_value[~below_cut] = 0
+    # return hod_value
     # + 0.5/Q*(1 + math.erf((logM_h-logM_cut-0.8)*3))
 
 def N_sat_ELG(M_h, logM_cut, kappa, logM_1, alpha, A_s=1.0, alpha1=0.0, beta=0.0):
@@ -93,11 +93,11 @@ def N_cen_QSO(M_h, logM_cut, sigma, p_max):
     # hod_value = np.ones(np.size(M_h))
     # hod_value[below_cut] = 0
     # hod_value[above_cut] = 0
-    below_cut = M_h < MASS_CUTOFF
-    hod_value = np.full(shape=len(M_h), fill_value=SMALL_NUMBER)
-    hod_value[below_cut] = 0
-    return hod_value
-    #return p_max * 0.5 * (1 + erf((np.log10(M_h) - logM_cut) / 1.41421356 / sigma)) # * 0 # For debugging; TODO: UNDO
+    # below_cut = M_h < MASS_CUTOFF # step function
+    # hod_value = np.full(shape=len(M_h), fill_value=SMALL_NUMBER)
+    # hod_value[below_cut] = 0
+    # return hod_value
+    return p_max * 0.5 * (1 + erf((np.log10(M_h) - logM_cut) / 1.41421356 / sigma)) # * 0 # For debugging; TODO: UNDO
     #return np.full(len(M_h), fill_value=SMALL_NUMBER * p_max)
 
 def N_sat_QSO(M_h, logM_cut, kappa, logM_1, alpha, A_s=1.0):

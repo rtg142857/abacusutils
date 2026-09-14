92e767ab52281046709bd0075f4fd370bdaa94b9
Alexander M. <rtg142857@gmail.com>
2026-09-14 13:47:55 +0100

    need to add the ELG cens back in (oh I do hope this works)

--- abacusnbody/hod/GRAND_HOD.py
+++ abacusnbody/hod/GRAND_HOD.py
@@ -113,7 +113,7 @@ def N_cen_ELG_v1(M_h, p_max, Q, logM_cut, sigma, gamma, Anorm=1):
     Q = np.inf
     return ((
         2.0 * (p_max - 1.0 / Q) * phi * Phi / Anorm
-    ) + 0.5/Q*(1 + math.erf((logM_h-logM_cut)/0.01))) * 0 # For debugging; TODO: UNDO # + 0.5/Q*(1 + math.erf((logM_h-logM_cut-0.8)*3))
+    ) + 0.5/Q*(1 + math.erf((logM_h-logM_cut)/0.01))) # + 0.5/Q*(1 + math.erf((logM_h-logM_cut-0.8)*3))
 
 
 @njit(fastmath=True)

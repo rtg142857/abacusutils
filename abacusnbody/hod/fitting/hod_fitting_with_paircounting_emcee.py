e2b0966af18bc65824448b616c74b8023d20e0ff
Alexander M. <rtg142857@gmail.com>
2026-09-18 16:10:40 +0100

    oops

--- abacusnbody/hod/fitting/hod_fitting_with_paircounting_emcee.py
+++ abacusnbody/hod/fitting/hod_fitting_with_paircounting_emcee.py
@@ -113,7 +113,7 @@ def sample_chain(target_wp_dict: dict, target_jackknife_inverse_dict: dict, targ
     #with Pool() as pool:
     start_time = time.time()
     print("Initialising sampler...", flush=True)
-    sampler = emcee.EnsembleSampler(nwalkers, ndim, setup.log_probability, backend=backend, pool=pool)
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, setup.log_probability, backend=backend)#, pool=pool)
 
     print("Running chain (serial)...", flush=True)
     sampler.run_mcmc(walker_init_pos, num_steps, skip_initial_state_check=True) # It feels like it likes to throw an error for the initial state check with the standard priors

e1df35147d9287680437fc9f629b0517003c7490
Alexander M. <rtg142857@gmail.com>
2026-09-18 15:45:13 +0100

    Trying with parallelisation now

--- abacusnbody/hod/fitting/hod_fitting_with_paircounting_emcee.py
+++ abacusnbody/hod/fitting/hod_fitting_with_paircounting_emcee.py
@@ -110,13 +110,14 @@ def sample_chain(target_wp_dict: dict, target_jackknife_inverse_dict: dict, targ
     walker_init_pos = param_set.get_initial_params(positions=nwalkers).T
     #walker_init_pos = initialise_walkers(initial_params_random=True,num_walkers=nwalkers)
 
-    start_time = time.time()
-    print("Initialising sampler...", flush=True)
-    sampler = emcee.EnsembleSampler(nwalkers, ndim, setup.log_probability, backend=backend)
+    with Pool(10) as pool:
+        start_time = time.time()
+        print("Initialising sampler...", flush=True)
+        sampler = emcee.EnsembleSampler(nwalkers, ndim, setup.log_probability, backend=backend)
 
-    print("Running chain (serial)...", flush=True)
-    sampler.run_mcmc(walker_init_pos, num_steps, skip_initial_state_check=True, progress=True) # It feels like it likes to throw an error for the initial state check with the standard priors
-    end_time = time.time()
+        print("Running chain (serial)...", flush=True)
+        sampler.run_mcmc(walker_init_pos, num_steps, skip_initial_state_check=True, pool=pool) # It feels like it likes to throw an error for the initial state check with the standard priors
+        end_time = time.time()
     print("fitting took ", end_time - start_time, " seconds", flush=True)
 
     return sampler

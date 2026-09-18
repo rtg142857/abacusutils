cc5123f8acfefc2c568501104f7d91f0233adad3
Alexander M. <rtg142857@gmail.com>
2026-09-18 15:55:40 +0100

    Debugging

--- abacusnbody/hod/fitting/hod_fitting_with_paircounting_emcee.py
+++ abacusnbody/hod/fitting/hod_fitting_with_paircounting_emcee.py
@@ -113,10 +113,10 @@ def sample_chain(target_wp_dict: dict, target_jackknife_inverse_dict: dict, targ
     with Pool(10) as pool:
         start_time = time.time()
         print("Initialising sampler...", flush=True)
-        sampler = emcee.EnsembleSampler(nwalkers, ndim, setup.log_probability, backend=backend)
+        sampler = emcee.EnsembleSampler(nwalkers, ndim, setup.log_probability, backend=backend, pool=pool)
 
         print("Running chain (serial)...", flush=True)
-        sampler.run_mcmc(walker_init_pos, num_steps, skip_initial_state_check=True, pool=pool) # It feels like it likes to throw an error for the initial state check with the standard priors
+        sampler.run_mcmc(walker_init_pos, num_steps, skip_initial_state_check=True) # It feels like it likes to throw an error for the initial state check with the standard priors
         end_time = time.time()
     print("fitting took ", end_time - start_time, " seconds", flush=True)
 

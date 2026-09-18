863b44aefbf35c4eb48441b233ddeabb068a869a
Alexander M. <rtg142857@gmail.com>
2026-09-18 19:49:46 +0100

    we'll get there eventually

--- scripts/hod/test_runs/test_paircounting.py
+++ scripts/hod/test_runs/test_paircounting.py
@@ -115,6 +115,7 @@ def main(path_config_filename):
     #if not pair_wp_exists:
 
     print("Loading paircounts from the tabulation mock", flush=True)###############################################
+    paircount_path = config["fitting_params"]["paircounts_save_path"] + sim_label + "/"
     paircounts = load_paircounts(paircount_path)
     other_stuff_dict_here = make_other_stuff_dict(boxsize=boxsize, num_sat_parts=3, subsample_dir=subsample_dir, sim_label=sim_label)
 
